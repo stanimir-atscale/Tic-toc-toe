acf19e8a27930f380dad53dd6401e7a369c03a96
stanimir.kolev <stanimir.kolev@atscale.com>
2021-11-16 14:37:52 +0200

    refactoring

--- tic-tac-toe/src/components/GameComponent/CellComponent/cell-component.test.tsx
+++ tic-tac-toe/src/components/GameComponent/CellComponent/cell-component.test.tsx
@@ -2,57 +2,51 @@ import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import GameStore from "../../../stores/GameStore";
 import { CellComponent } from "./CellComponent";
+import { observable } from "mobx";
 
 Enzyme.configure({ adapter: new Adapter() });
+const mockCellIndex = 0;
+let store: GameStore;
+let mockProps: {
+  playerId: number | null;
+  cellIndex: number;
+  currentPlayerMark: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
 
 describe("GameComponent", () => {
+  beforeEach(() => {
+    store = new GameStore();
+    mockProps = {
+      playerId: store.board.cells[mockCellIndex],
+      cellIndex: mockCellIndex,
+      currentPlayerMark: "x",
+      onChange: function () {
+        store.checkboxChange(mockCellIndex);
+      },
+    };
+  });
   it("Should check mark onto hovered cell", () => {
-    const mockProps = new MockProps();
-    const component = shallow(
-      <CellComponent {...mockProps.getMockedProps()} />
-    );
+    const component = shallow(<CellComponent {...mockProps} />);
     component.find(".ttt-label").simulate("mouseOver");
     expect(component.find(".label--x").exists()).toBe(true);
   });
   it("Should check mark onto clicked cell", () => {
-    const mockProps = new MockProps();
-    const component = shallow(
-      <CellComponent {...mockProps.getMockedProps()} />
-    );
+    const component = shallow(<CellComponent {...mockProps} />);
     component.find("input").simulate("change", { target: { checked: true } });
     component.setProps({
-      playerId: mockProps.store.board.cells[mockProps.mockCellIndex],
+      playerId: store.board.cells[mockCellIndex],
     });
     expect(component.find(".mark--x").exists()).toBe(true);
   });
   it("Should check mark is changed after second click on the same cell", () => {
-    const mockProps = new MockProps();
-    const component = shallow(
-      <CellComponent {...mockProps.getMockedProps()} />
-    );
+    const component = shallow(<CellComponent {...mockProps} />);
 
     component.find("input").simulate("change", { target: { checked: true } });
     component.setProps({
-      playerId: mockProps.store.board.cells[mockProps.mockCellIndex],
+      playerId: store.board.cells[mockCellIndex],
     });
     expect(component.find(".mark--x").exists()).toBe(true);
     expect(component.find("input").props().checked).toBe(true);
   });
 });
-
-class MockProps {
-  mockCellIndex = 0;
-  store = new GameStore();
-
-  getMockedProps() {
-    let that = this;
-    return {
-      playerId: this.store.board.cells[this.mockCellIndex],
-      cellIndex: this.mockCellIndex,
-      currentPlayerMark: "x",
-      onChange: function () {
-        that.store.checkboxChange(that.mockCellIndex);
-      },
-    };
-  }
-}
