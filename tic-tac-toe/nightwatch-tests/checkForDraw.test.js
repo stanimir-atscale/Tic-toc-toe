c9d925b70fd1fd7cb0440e6345e0783984d200e4
stanimir.kolev <stanimir.kolev@atscale.com>
2021-11-16 17:12:01 +0200

    refactoring e2e tests

--- tic-tac-toe/nightwatch-tests/checkForDraw.test.js
+++ tic-tac-toe/nightwatch-tests/checkForDraw.test.js
@@ -2,19 +2,19 @@ module.exports = {
     "Draw and check results"(browser) {
       const firstPlayerWinsEl = ".ttt-game-info-container .ttt-game-info-component:first-child .ttt-player-wins";
       const secondPlayerWinsEl = ".ttt-game-info-container .ttt-game-info-component:last-child .ttt-player-wins";
-      let areWinChange = false;
+      let areWinsChange = false;
       let firstPlayerWins;
       let secondPlayerWins;      
 
       browser
         .url("http://localhost:3000/")
         .waitForElementVisible(".ttt-game-component")
-          .getText(firstPlayerWinsEl, function(winsObj) {
+        .getText(firstPlayerWinsEl, function(winsObj) {
             firstPlayerWins = parseInt(winsObj.value);
-          })
-          .getText(secondPlayerWinsEl, function(winsObj) {
+        })
+        .getText(secondPlayerWinsEl, function(winsObj) {
             secondPlayerWins = parseInt(winsObj.value);
-          })
+        })
         .click(".cell--0 label")
         .click(".cell--1 label")
         .click(".cell--4 label")
@@ -22,22 +22,22 @@ module.exports = {
         .click(".cell--5 label")
         .click(".cell--3 label")
         .click(".cell--6 label")
-        .click(".cell--7 label")
         .click(".cell--8 label")
+        .click(".cell--7 label")
         .waitForElementVisible(".ttt-endgame")
         .click(".ttt-tryAgain")
         .getText(firstPlayerWinsEl, function(winsObj) {
           if (firstPlayerWins !== parseInt(winsObj.value)) {
-            areWinChange = true;
+            areWinsChange = true;
           }
         })
         .getText(secondPlayerWinsEl, function(winsObj) {
           if (secondPlayerWins !== parseInt(winsObj.value)) {
-            areWinChange = true;
+            areWinsChange = true;
           }
         })
         .perform(function () {
-          this.assert.ok(!areWinChange, "Wins are correct!");
+          this.assert.ok(!areWinsChange, "Wins count for draw are correct!");
         })
         .end();
     }
